4282324ed7891f3bded17946615efd1ab4d43409
Gordon Euller <gordon@euller.at>
2017-04-24 18:42:47 +0200

    first seed draft

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,18 @@
+
+users = User.create([
+  {
+    name: "Hans", email: "hans@test.de", password: "abcdef"
+  }, {
+    name: "Megan", email: "megan@test.de", password: "defghi"
+  }]
+)
+
+venues = Venue.create([{
+  address: "Hauptstrasse 2", capacity: 200, category: "flat", price: 500,
+  user_id: 2, name: "Fusion Factory"}])
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
