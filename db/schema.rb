8037a4981c9aaea40135866af4005c80d130af45
Fredrik Forstbach <fredrik.forstbach@gmail.com>
2017-04-24 14:41:06 +0200

    added booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424104420) do
+ActiveRecord::Schema.define(version: 20170424123731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "venues", force: :cascade do |t|
-    t.string   "address"
-    t.integer  "capacity"
-    t.string   "category"
-    t.integer  "price"
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start"
+    t.datetime "end"
+    t.integer  "user_id"
+    t.integer  "venue_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+    t.index ["venue_id"], name: "index_bookings_on_venue_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -38,7 +41,17 @@ ActiveRecord::Schema.define(version: 20170424104420) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
   end
 
+  create_table "venues", force: :cascade do |t|
+    t.string   "address"
+    t.integer  "capacity"
+    t.string   "category"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "bookings", "users"
+  add_foreign_key "bookings", "venues"
 end
