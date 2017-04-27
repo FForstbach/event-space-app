a786e84a44d0f7e27b695e798843c6b16a3dc5a4
Fredrik Forstbach <fredrik.forstbach@gmail.com>
2017-04-27 12:25:51 +0200

    added schema rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426091922) do
+ActiveRecord::Schema.define(version: 20170427102205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170426091922) do
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.boolean  "booking_confirmed", default: false
+    t.integer  "guests"
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
     t.index ["venue_id"], name: "index_bookings_on_venue_id", using: :btree
   end
